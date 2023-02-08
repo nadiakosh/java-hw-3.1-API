6a45aa40a7e8dc7166376c066f1d295f0b30f712
Nadia <nadya-koshelkovskaya@yandex.ru>
2023-02-08 19:17:22 +0300

    Initial commit

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,8 @@
+package ru.Netology;
+
+public class FormDate {
+    public int day = 13;
+    public int month = 6;
+    public int year = 1999;
+
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,12 @@
+package ru.Netology;
+
+public class Post {
+    public String name = "Иван";
+    public String passport = "4444 № 44444444 ";
+    public String patronymic = "Иванович";
+    public String phone = "+7 (999)-999-99-99";
+    public String surname = "Иванов";
+    public boolean subscription = true;
+    public FormDate birthday;
+
+}
